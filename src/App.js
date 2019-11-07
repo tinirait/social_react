7acb0d8ca2e987854717fd0ddad293399fd370c6
Apple <apple@MacBook-Pro-Apple.local>
2019-11-07 18:00:41 +0300

    social-grid-step-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import './App.css';
-import Technologies from "./Technologies";
-import Footer from "./Footer";
-import Header from "./Header";
 
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://img2.gratispng.com/20171216/258/fox-png-5a35a82f32c449.143206221513465903208.jpg'
+                     alt='Fox'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href='/'>Profile</a>
+                </div>
+                <div>
+                    <a href='/'>Messages</a>
+                </div>
+                <div>
+                    <a href='/'>News</a>
+                </div>
+                <div>
+                    <a href='/'>Music</a>
+                </div>
+                <div>
+                    <a href='/'>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoJamjwVDfdabVrW1Vz4Md3sSAFTMNuynBds_1srQyebuPRTqh"
+                        alt="Test"/>
+                </div>
+
+                <div>Ava+description</div>
+                <div>
+                    My posts
+                    <div>News Posts</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+
+            </div>
         </div>
     );
 };
