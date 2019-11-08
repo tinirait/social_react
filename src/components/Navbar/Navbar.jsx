54662fe3d89ecceaeeae2477e2d1e06bdac450c3
Apple <apple@MacBook-Pro-Apple.local>
2019-11-08 22:49:39 +0300

    20:React-navlik-touter

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import classes from'./Navbar.module.css'
+import {NavLink} from "react-router-dom";
 const NavBar = () => {
     return (
         <nav className={classes.nav}>
-            <div className = {`${classes.item} ${classes.active}`}>
-                <a  className={classes.activeLinkEllow} href='/'>Profile</a>
+            <div className = {classes.item}>
+                <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink></div>
+            <div className ={classes.item}>
+                <NavLink  to='/dialogs' activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className ={classes.item}>
-                <a  href='/'>Messages</a>
+                <NavLink  to='/news' activeClassName={classes.activeLink}>News</NavLink>
             </div>
             <div className ={classes.item}>
-                <a  href='/'>News</a>
+                <NavLink  to='/music' activeClassName={classes.activeLink}>Music</NavLink>
             </div>
             <div className ={classes.item}>
-                <a  href='/'>Music</a>
-            </div>
-            <div className ={classes.itemSettings}>
-                <a  href='/'>Settings</a>
+                <NavLink  to='/settings' activeClassName={classes.activeLink}>Settings</NavLink>
             </div>
         </nav>
     );
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import classes from './Dialogs.module.css'
+
+const Settings = (props) => {
+    return (
+        <div>
+        Settings
+        </div>
+    );
+};
+
+export default Settings;
