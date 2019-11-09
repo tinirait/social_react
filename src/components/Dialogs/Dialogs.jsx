7b3644da3f6a5279a6bda746bbba83eef3a6b690
Apple <apple@MacBook-Pro-Apple.local>
2019-11-09 14:09:34 +0300

    microVerstka

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-// import classes from './Dialogs.module.css'
+import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-        Dialogs
+
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.itemsName + ' ' + classes.active}>Dima</div>
+                <div className={classes.itemsName}>Vlad</div>
+                <div className={classes.itemsName}>Victor</div>
+                <div className={classes.itemsName}>Farid</div>
+                <div className={classes.itemsName}>Vitalik</div>
+                <div className={classes.itemsName}>Alex</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.itemsMessage}>Hi test?</div>
+                <div className={classes.itemsMessage}>Why test?</div>
+                <div className={classes.itemsMessage}>Yo test?</div>
+
+            </div>
         </div>
+
     );
 };
 
